6e4182e7a14dc673da55d91c78835d52140a6b45
savioruz <kheril.dev@gmail.com>
2025-05-30 08:51:55 +0700

    feat(locations): add timezone parameter to connection builder for improved database configuration

--- pkg/postgres/builder.go
+++ pkg/postgres/builder.go
@@ -4,14 +4,15 @@ import (
 	"fmt"
 )
 
-func ConnectionBuilder(host string, port int, user, password, dbName, sslMode string) string {
-	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+func ConnectionBuilder(host string, port int, user, password, dbName, sslMode, timezone string) string {
+	dsn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s TimeZone=%s",
 		host,
 		port,
 		user,
 		password,
 		dbName,
 		sslMode,
+		timezone,
 	)
 
 	return dsn
